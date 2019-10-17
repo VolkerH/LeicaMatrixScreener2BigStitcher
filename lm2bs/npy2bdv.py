7b565e7d7eb559762182b0c9abe10cdea78df371
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2019-10-17 23:30:28 +1100

    first upload of existing code base from private gitlab

---
+++ lm2bs/npy2bdv.py
@@ -0,0 +1,331 @@
+# bundling Nikita Vladimirov's code here to simplify distribution
+# The original repo can be found at
+# https://github.com/nvladimus/npy2bdv
+# and may be more current.
+
+#
+# Fast writing of numpy arrays to HDF5 format compatible with Fiji/BigDataViewer and BigStitcher
+# Inspired by https://github.com/tlambert03/imarispy and Adam Glaser code
+# Author: Nikita Vladimirov
+# MIT license
+import os
+import h5py
+import numpy as np
+from xml.etree import ElementTree as ET
+import skimage.transform
+
+
+class BdvWriter:
+    def __init__(self, filename,
+                 subsamp=((1, 1, 1),),
+                 blockdim=((4, 256, 256),),
+                 compression=None,
+                 nilluminations=1, nchannels=1, ntiles=1, nangles=1):
+        """Class for writing multiple numpy 3d-arrays into BigDataViewer/BigStitcher HDF5 file.
+
+        Parameters:
+            filename: (string) full path to a new file
+            subsamp: (tuple of tuples),
+                Subsampling levels in (z,y,x) order. Integers >= 1, default value ((1, 1, 1),)
+            blockdim: (tuple of tuples),
+                Block size for h5 storage, in pixels, in (z,y,x) order. Default ((4,256,256),), see notes.
+            compression: str
+                (None, 'gzip', 'lzf'), HDF5 compression method. Default is None for high-speed writing.
+            nilluminations, nchannels, ntiles, nangles, (int)
+                number of view attributes, default 1.
+
+        Notes:
+        Input stacks and output files are assumed uint16 type.
+
+        The h5 recommended block (chunk) size should be between 10 KB and 1 MB, larger for large arrays.
+        For example, block dimensions (4,256,256)px gives ~0.5MB block size for type int16 (2 bytes) and writes very fast.
+        Block size can be larger than stack dimension.
+        """
+        assert nilluminations >= 1, "Total number of illuminations must be at least 1."
+        assert nchannels >= 1, "Total number of channels must be at least 1."
+        assert ntiles >= 1, "Total number of tiles must be at least 1."
+        assert nangles >= 1, "Total number of angles must be at least 1."
+        assert compression in (None, 'gzip', 'lzf'), 'Unknown compression type'
+        assert not os.path.exists(filename), "File already exists, writing terminated"
+        assert all([isinstance(element, int) for tupl in subsamp for element in
+                    tupl]), 'subsamp values should be integers >= 1.'
+        if len(blockdim) < len(subsamp):
+            print("Number of blockdim levels (" + str (len(blockdim)) +
+                  ") is less than subsamp levels (" + str(len(subsamp)) + ")\n" +
+                  "First-level block size " + str(blockdim[0]) + " will be used for all levels\n")
+
+        self.nsetups = nilluminations * nchannels * ntiles * nangles
+        self.nilluminations = nilluminations
+        self.nchannels = nchannels
+        self.ntiles = ntiles
+        self.nangles = nangles
+        self.subsamp = np.asarray(subsamp)
+        self.chunks = self.compute_chunk_size(blockdim)
+        self.stack_shapes = {}
+        self.affine_matrices = {}
+        self.affine_names = {}
+        self.calibrations = {}
+        self.voxel_size_xyz = {}
+        self.voxel_units = {}
+        self.exposure_time = {}
+        self.exposure_units = {}
+        self.compression = compression
+        self.filename = filename
+        self.file_object = h5py.File(filename, 'a')
+        self.write_setups_header()
+        self.__version__ = "2019.09.17"
+
+    def write_setups_header(self):
+        """Write resolutions and subdivisions for all setups into h5 file."""
+        for isetup in range(self.nsetups):
+            grp = self.file_object.create_group('s{:02d}'.format(isetup))
+            data_subsamp = np.flip(self.subsamp, 1)
+            data_chunks = np.flip(self.chunks, 1)
+            grp.create_dataset('resolutions', data=data_subsamp, dtype='<f8')
+            grp.create_dataset('subdivisions', data=data_chunks, dtype='<i4')
+
+    def append_view(self, stack, time, illumination=0, channel=0, tile=0, angle=0,
+                    m_affine=None, name_affine='manually defined',
+                    voxel_size_xyz=(1, 1, 1), voxel_units='px', calibration=(1, 1, 1),
+                    exposure_time=0, exposure_units='s'):
+        """Write numpy 3-dimensional array (stack) to h5 file at specified timepint (itime) and setup number (isetup).
+        Parameters:
+            stack: numpy array (uint16)
+                3-dimensional stack of data in (z,y,x) axis order.
+            time: (int)
+                time index, starting from 0.
+            illumination, channel, view, angle: (int)
+                indices of the view attributes, starting from 0.
+            m_affine: a (3,4) numpy array, optional
+                Coefficients of affine transformation matrix (m00, m01m ...)
+            name_affine: str, optional
+                Name of affine transformation
+            voxel_size_xyz: tuple of 3 elements, optional
+                The physical size of voxel, in voxel_units. Default (1, 1, 1).
+            voxel_units: str, optional
+                spatial units (default is 'px').
+            calibration: tuple of 3 elements, optional
+                The anisotropy factors for (x,y,z) voxel calibration. Default (1, 1, 1).
+                Leave it default unless you are expert in BigStitcher and know how it affects transformations.
+            exposure_time: scalar, optional
+                Camera exposure time for this view, default 0.
+            exposure_units: str, optional
+                Time units for this view, default "s".
+        """
+        assert len(stack.shape) == 3, "Stack should be a 3-dimensional numpy array (z,y,x)"
+        assert len(calibration) == 3, "Calibration must be a tuple of 3 elements (x, y, z)."
+        assert len(voxel_size_xyz) == 3, "Voxel size must be a tuple of 3 elements (x, y, z)."
+        fmt = 't{:05d}/s{:02d}/{}'
+        nlevels = len(self.subsamp)
+        isetup = self.determine_setup_id(illumination, channel, tile, angle)
+        self.stack_shapes[isetup] = stack.shape
+        for ilevel in range(nlevels):
+            grp = self.file_object.create_group(fmt.format(time, isetup, ilevel))
+            subdata = self.subsample_stack(stack, self.subsamp[ilevel]).astype('int16')
+            grp.create_dataset('cells', data=subdata, chunks=self.chunks[ilevel],
+                               maxshape=(None, None, None), compression=self.compression)
+        if m_affine is not None:
+            self.affine_matrices[isetup] = m_affine
+            self.affine_names[isetup] = name_affine
+        self.calibrations[isetup] = calibration
+        self.voxel_size_xyz[isetup] = voxel_size_xyz
+        self.voxel_units[isetup] = voxel_units
+        self.exposure_time[isetup] = exposure_time
+        self.exposure_units[isetup] = exposure_units
+
+    def compute_chunk_size(self, blockdim):
+        """Populate the size of h5 chunks.
+        Use first-level chunk size if there are more subsampling levels than chunk size levels.
+        """
+        chunks = []
+        base_level = blockdim[0]
+        if len(blockdim) < len(self.subsamp):
+            for ilevel in range(len(self.subsamp)):
+                chunks.append(base_level)
+            chunks_tuple = tuple(chunks)
+        else:
+            chunks_tuple = blockdim
+        return chunks_tuple
+
+    def subsample_stack(self, stack, subsamp_level):
+        """Subsampling of 3d stack.
+        Parameters:
+            stack, numpy 3d array (z,y,x) of int16
+            subsamp_level, array-like with 3 elements, eg (2,4,4) for downsampling z(x2), x and y (x4).
+        Return:
+            down-scaled stack, unit16 type.
+        """
+        if all(subsamp_level[:] == 1):
+            stack_sub = stack
+        else:
+            stack_sub = skimage.transform.downscale_local_mean(stack, tuple(subsamp_level)).astype(np.uint16)
+        return stack_sub
+
+    def write_xml_file(self, ntimes=1,
+                       camera_name="default",  microscope_name="default",
+                       microscope_version="0.0", user_name="user"):
+        """
+        Write XML header file for the HDF5 file.
+
+        Parameters:
+            ntimes: int
+                number of time points
+            camera_name: str, optional
+                Name of the camera (same for all setups at the moment, ToDo)
+            microscope_name: str, optional
+            microscope_version: str, optional
+            user_name: str, optional
+        """
+        assert ntimes >= 1, "Total number of time points must be at least 1."
+        root = ET.Element('SpimData')
+        root.set('version', '0.2')
+        bp = ET.SubElement(root, 'BasePath')
+        bp.set('type', 'relative')
+        bp.text = '.'
+        # new XML data, added by @nvladimus
+        generator = ET.SubElement(root, 'generatedBy')
+        library = ET.SubElement(generator, 'library')
+        library.set('version', self.__version__)
+        library.text = "npy2bdv"
+        microscope = ET.SubElement(generator, 'microscope')
+        ET.SubElement(microscope, 'name').text = microscope_name
+        ET.SubElement(microscope, 'version').text = microscope_version
+        ET.SubElement(microscope, 'user').text = user_name
+        # end of new XML data
+
+        seqdesc = ET.SubElement(root, 'SequenceDescription')
+        imgload = ET.SubElement(seqdesc, 'ImageLoader')
+        imgload.set('format', 'bdv.hdf5')
+        el = ET.SubElement(imgload, 'hdf5')
+        el.set('type', 'relative')
+        el.text = os.path.basename(self.filename)
+        # write ViewSetups
+        viewsets = ET.SubElement(seqdesc, 'ViewSetups')
+        for iillumination in range(self.nilluminations):
+            for ichannel in range(self.nchannels):
+                for itile in range(self.ntiles):
+                    for iangle in range(self.nangles):
+                        isetup = self.determine_setup_id(iillumination, ichannel, itile, iangle)
+                        vs = ET.SubElement(viewsets, 'ViewSetup')
+                        ET.SubElement(vs, 'id').text = str(isetup)
+                        ET.SubElement(vs, 'name').text = 'setup ' + str(isetup)
+                        nz, ny, nx = tuple(self.stack_shapes[isetup])
+                        ET.SubElement(vs, 'size').text = '{} {} {}'.format(nx, ny, nz)
+                        vox = ET.SubElement(vs, 'voxelSize')
+                        ET.SubElement(vox, 'unit').text = self.voxel_units[isetup]
+                        dx, dy, dz = self.voxel_size_xyz[isetup]
+                        ET.SubElement(vox, 'size').text = '{} {} {}'.format(dx, dy, dz)
+                        # new XML data, added by @nvladimus
+                        cam = ET.SubElement(vs, 'camera')
+                        ET.SubElement(cam, 'name').text = camera_name
+                        ET.SubElement(cam, 'exposureTime').text = '{}'.format(self.exposure_time[isetup])
+                        ET.SubElement(cam, 'exposureUnits').text = self.exposure_units[isetup]
+                        # end of new XML data
+                        a = ET.SubElement(vs, 'attributes')
+                        ET.SubElement(a, 'illumination').text = str(iillumination)
+                        ET.SubElement(a, 'channel').text = str(ichannel)
+                        ET.SubElement(a, 'tile').text = str(itile)
+                        ET.SubElement(a, 'angle').text = str(iangle)
+
+        # write Attributes (range of values)
+        attrs_illum = ET.SubElement(viewsets, 'Attributes')
+        attrs_illum.set('name', 'illumination')
+        for iilumination in range(self.nilluminations):
+            illum = ET.SubElement(attrs_illum, 'Illumination')
+            ET.SubElement(illum, 'id').text = str(iilumination)
+            ET.SubElement(illum, 'name').text = 'illumination ' + str(iilumination)
+
+        attrs_chan = ET.SubElement(viewsets, 'Attributes')
+        attrs_chan.set('name', 'channel')
+        for ichannel in range(self.nchannels):
+            chan = ET.SubElement(attrs_chan, 'Channel')
+            ET.SubElement(chan, 'id').text = str(ichannel)
+            ET.SubElement(chan, 'name').text = 'channel ' + str(ichannel)
+
+        attrs_tile = ET.SubElement(viewsets, 'Attributes')
+        attrs_tile.set('name', 'tile')
+        for itile in range(self.ntiles):
+            tile = ET.SubElement(attrs_tile, 'Tile')
+            ET.SubElement(tile, 'id').text = str(itile)
+            ET.SubElement(tile, 'name').text = 'tile ' + str(itile)
+
+        attrs_ang = ET.SubElement(viewsets, 'Attributes')
+        attrs_ang.set('name', 'angle')
+        for iangle in range(self.nangles):
+            ang = ET.SubElement(attrs_ang, 'Angle')
+            ET.SubElement(ang, 'id').text = str(iangle)
+            ET.SubElement(ang, 'name').text = 'angle ' + str(iangle)
+
+        # Time points
+        tpoints = ET.SubElement(seqdesc, 'Timepoints')
+        tpoints.set('type', 'range')
+        ET.SubElement(tpoints, 'first').text = str(0)
+        ET.SubElement(tpoints, 'last').text = str(ntimes - 1)
+
+        # Transformations of coordinate system
+        vregs = ET.SubElement(root, 'ViewRegistrations')
+
+        for itime in range(ntimes):
+            for iset in range(self.nsetups):
+                vreg = ET.SubElement(vregs, 'ViewRegistration')
+                vreg.set('timepoint', str(itime))
+                vreg.set('setup', str(iset))
+                # write arbitrary affine transformation, specific for each view
+                if iset in self.affine_matrices.keys():
+                    vt = ET.SubElement(vreg, 'ViewTransform')
+                    vt.set('type', 'affine')
+                    ET.SubElement(vt, 'Name').text = self.affine_names[iset]
+                    n_prec = 6
+                    mx_string = np.array2string(self.affine_matrices[iset].flatten(), separator=' ',
+                                                precision=n_prec, floatmode='fixed',
+                                                max_line_width=(n_prec+5)*4)
+                    ET.SubElement(vt, 'affine').text = mx_string[1:-1].strip()
+
+                # write registration transformation (calibration)
+                vt = ET.SubElement(vreg, 'ViewTransform')
+                vt.set('type', 'affine')
+                ET.SubElement(vt, 'Name').text = 'calibration'
+                calx, caly, calz = self.calibrations[iset]
+                ET.SubElement(vt, 'affine').text = \
+                    '{} 0.0 0.0 0.0 0.0 {} 0.0 0.0 0.0 0.0 {} 0.0'.format(calx, caly, calz)
+
+        self.xml_indent(root)
+        tree = ET.ElementTree(root)
+        tree.write(os.path.splitext(self.filename)[0] + ".xml", xml_declaration=True, encoding='utf-8', method="xml")
+        return
+
+    def xml_indent(self, elem, level=0):
+        """Pretty printing function"""
+        i = "\n" + level * "  "
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "  "
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+            for elem in elem:
+                self.xml_indent(elem, level + 1)
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+
+    def determine_setup_id(self, illumination=0, channel=0, tile=0, angle=0):
+        """Takes the view attributes (illumination, channel, tile, angle) and converts them into unique setup_id.
+        Parameters:
+            illumination (int) >=0
+            channel (int) >= 0
+            tile (int) >= 0
+            angle (int) >= 0
+        Returns
+            setup_id (int), starting from 0 (first setup)
+            """
+        setup_id_matrix = np.arange(self.nsetups)
+        setup_id_matrix = setup_id_matrix.reshape((self.nilluminations, self.nchannels, self.ntiles, self.nangles))
+        setup_id = setup_id_matrix[illumination, channel, tile, angle]
+        return setup_id
+
+    def close(self):
+        """Close the file object."""
+        self.file_object.close()
+
---
+++ lm2bs/process_matrix_screener_data.py
@@ -0,0 +1,327 @@
+# Tools for creating a BigStitcher/BigDataViewer .xml/h5 project file
+# from Leica Matrix Screener .ome.tiff output
+# 
+# Volker . Hilsenstein @ Monash dot Edu
+# Sep/Oct 2019
+# License BSD-3
+
+import pathlib
+import tifffolder
+import pandas as pd
+import numpy as np
+import tifffile
+import re
+import h5py
+from typing import Tuple, Union, List
+from skimage.measure import block_reduce
+from copytree import copytree
+import tqdm
+import time
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+import npy2bdv
+
+
+def split_pathname(filename: str) -> pd.Series:
+    """extracts u,v (well) and x,y (field) coordinates from a matrix screener file name
+    
+    Parameters
+    ----------
+    filename : str
+        filename of Leica matrix screener tif file 
+    
+    Returns
+    -------
+    pd.Series
+        pandas series object with u,v,x,y keys
+    """
+    regex = ".*--U(?P<u>\d+)--V(?P<v>\d+).*--X(?P<x>\d+)--Y(?P<y>\d+)"
+    m = re.match(regex, str(filename))
+    if m is None:
+        raise (RuntimeError, "folder not matching regular expression pattern")
+    return pd.to_numeric(pd.Series(m.groupdict()))
+
+
+def get_meta_from_matrix_ome_tif(filename):
+    """ given an ome tif file produced by Leica Matrix Screener,
+    return a dictionary with some of the metadata
+    """
+    tfile = tifffile.TiffFile(filename)
+    # print(tfile)
+    # note: previous versions of tifffile required
+    # thr following step:cd
+    # _meta = xmltodict.parse(tfile.ome_metadata)
+    _meta = tfile.ome_metadata
+    meta = {}
+    meta["Size X"] = _meta["Image"]["Pixels"]["SizeX"]
+    meta["Size Y"] = _meta["Image"]["Pixels"]["SizeY"]
+    meta["PhysicalSize X"] = float(_meta["Image"]["Pixels"]["PhysicalSizeX"])
+    meta["PhysicalSize Y"] = float(_meta["Image"]["Pixels"]["PhysicalSizeY"])
+    meta["Stage X"] = float(
+        _meta["Image"]["Pixels"]["Plane"]["StagePosition"]["PositionX"]
+    )
+    meta["Stage Y"] = float(
+        _meta["Image"]["Pixels"]["Plane"]["StagePosition"]["PositionY"]
+    )
+    return meta
+
+
+def get_field(field):
+    """ for a given field folder of the leica matrix screener 
+    
+    Read the stack and return 
+      * a numpy like object (Tifffolder)
+      * a dictionary with metadata information required for the affine transform matrix
+    """
+    np_like_array = tifffolder.TiffFolder(field, {"z": "--Z{d2}"})
+    first_file = np_like_array.files[0]
+    meta = get_meta_from_matrix_ome_tif(first_file)
+    return np_like_array, meta
+
+
+def save_files_for_bigstitcher(
+    matrix_screener_fields,
+    projected=True,
+    volume=True,
+    *,
+    h5_proj_name=None,
+    h5_vol_name=None,
+    zspacing=1.0,
+    project_func=np.max,
+    direction_x=-1,
+    direction_y=1,
+):
+    """
+    Save the fields in matrix screener fields as BigStitcher projects
+
+    if volume is True, a project for stitching volumes is created
+    if projection is True, a project for stitching projections is creates
+    h5_*_name are the outputfilenames for the volume and projection projects
+    zspacing is the spacing between z slices in um (cannot find this in metadata)
+    project_func is the aggregation function for projections
+    direction_* should be either +1 or -1 and can be used to flip coordinate 
+    system directions
+    """
+    print(f"Zspacing: {zspacing}")
+    if projected:
+        assert h5_proj_name is not None, "h5 output file for projections must be provided"
+        bdv_proj_writer = npy2bdv.BdvWriter(
+            h5_proj_name,
+            nchannels=1,
+            ntiles=len(matrix_screener_fields),
+            subsamp=((1, 1, 1), (1, 2, 2), (1, 4, 4), (1, 8, 8), (1, 16, 16)),
+            blockdim=((1, 64, 64),),
+            compression="gzip",
+        )  # , (4,4,1)))
+
+    if volume:
+        assert h5_vol_name is not None, "h5 output file for volumes must be provided"
+        bdv_vol_writer = npy2bdv.BdvWriter(
+            h5_vol_name,
+            nchannels=1,
+            ntiles=len(matrix_screener_fields),
+            subsamp=(
+                (1, 1, 1),
+                (1, 2, 2),
+                (1, 4, 4),
+                (1, 8, 8),
+                (2, 16, 16),
+                (4, 32, 32),
+            ),
+            blockdim=(
+                (64, 64, 64),
+                (64, 64, 64),
+                (64, 64, 64),
+                (64, 64, 64),
+                (32, 32, 32),
+                (16, 16, 16),
+            ),
+            compression="gzip",
+        )
+
+    affine_matrix_template = np.array(
+        ((1.0, 0.0, 0.0, 0.0), (0.0, 1.0, 0.0, 0.0), (0.0, 0.0, 1.0, 0.0))
+    )
+
+    for tile_nr, field in enumerate(matrix_screener_fields):
+        print(f"Processing {tile_nr+1} out of {len(matrix_screener_fields)}:")
+        print(field)
+        stack, meta = get_field(field)
+        affine = affine_matrix_template.copy()
+        # Explanation for formula below:
+        # Stage position in metadata appears to be in units of metres (m)
+        # PhysicalSize appears to be micrometers per voxel (um/vox)
+        # therefore for the stageposition in voxel coordinates we need to
+        # scale from meters to um (factor 1000000) and then divide by um/vox
+        # the direction vectors should be either 1 or -1 and can be used
+        # to flip the direction of the coordinate axes.
+        affine[1, 3] = (
+            meta["Stage X"] * 1_000_000 / meta["PhysicalSize X"] * direction_x
+        )  # -2247191 #-2_000_000
+        affine[0, 3] = (
+            meta["Stage Y"] * 1_000_000 / meta["PhysicalSize Y"] * direction_y
+        )  # 2247191 #2_000_000
+
+        if volume:
+            bdv_vol_writer.append_view(
+                stack,
+                time=0,
+                channel=0,
+                m_affine=affine,
+                tile=tile_nr,
+                name_affine=f"tile {tile_nr} translation",
+                voxel_size_xyz=(meta["PhysicalSize X"], meta["PhysicalSize Y"], zspacing),
+                voxel_units="um",
+                calibration=(1, 1, zspacing/meta["PhysicalSize X"]),
+            )
+        if projected:
+            outstack = np.expand_dims(project_func(stack, axis=0), axis=0)
+            bdv_proj_writer.append_view(
+                outstack,
+                time=0,
+                channel=0,
+                m_affine=affine,
+                tile=tile_nr,
+                name_affine=f"proj. tile {tile_nr} translation",
+                # Projections are inherently 2D, so we just repeat the X voxel size for Z
+                voxel_size_xyz=(
+                    meta["PhysicalSize X"],
+                    meta["PhysicalSize Y"],
+                    meta["PhysicalSize X"],
+                ),
+                voxel_units="um",
+                # calibration=(1, 1, 1),
+            )
+
+    if projected:
+        bdv_proj_writer.write_xml_file(ntimes=1)
+        bdv_proj_writer.close()
+    if volume:
+        bdv_vol_writer.write_xml_file(ntimes=1)
+        bdv_vol_writer.close()
+
+
+class Matrix_Mosaic_Processor(object):
+    """Holds state and methods to convert files from a Matrix Screener scan for use in BigStitcher 
+    """
+
+    def __init__(self, f: Union[str, pathlib.Path]) -> None:
+        """initialiaze a Matrix_Mosaic_Processor at the given Matrix Screener folder Path
+        
+        Parameters
+        ----------
+        f : Union[str, pathlib.Path]
+            Folder location where the matrix screener output files are.
+        """
+        self.matrix_folder: pathlib.Path = pathlib.Path(f)
+        self.df, self.uvwells = self._populate_file_df()
+
+    def __str__(self) -> str:
+        r = "Unique wells:\n"
+        for i, well in enumerate(self.uvwells):
+            r += str(i).zfill(3) + f": {well}\n"
+        return r
+
+    def _populate_file_df(self) -> Tuple[pd.DataFrame, list]:
+        """ populates data frame with all fields of view comprising the matrix scan experiment
+        
+        Returns
+        -------
+        Tuple[pd.DataFrame, list]
+            Returns a tuple consisting of a data frame of all fields of view as well 
+            as a list of unique (u,v) - well combinations 
+        """
+        # find all field-- folders
+        print("finding fields recurively")
+        field_cands = self.matrix_folder.rglob("field*")
+        fields = list(filter(lambda x: x.is_dir(), field_cands))
+        if fields == []:
+            return pd.DataFrame, []
+        # find unique chamber-- folders
+        chambers = set([f.parent for f in fields])
+        # populate data frame
+        df = pd.DataFrame(
+            data=[map(lambda x: str(x), fields), map((lambda x: str(x.parent)), fields)]
+        ).transpose()
+        df.columns = ["field", "chamber"]
+        df[["u", "v", "x", "y"]] = df["field"].apply(split_pathname)
+        df = df.sort_values(["u", "v", "x", "y"])
+        # find unique combinations of u,v
+        # https://stackoverflow.com/questions/35268817/unique-combinations-of-values-in-selected-columns-in-pandas-data-frame-and-count
+
+        uvcombos = (
+            df.groupby(["u", "v"]).size().reset_index().rename(columns={0: "count"})
+        )
+        uvcombos = uvcombos.sort_values(["u", "v"])
+        uvwells = list(zip(uvcombos.u, uvcombos.v))
+        print(uvwells)
+        return df, uvwells
+
+    def process_well(
+        self,
+        wellindex: int,
+        outfolder_base: pathlib.Path,
+        projected: bool,
+        volume: bool,
+        zspacing: float,
+    ):
+
+        u, v = self.uvwells[wellindex]
+        h5_proj_name, h5_vol_name = None, None
+
+        print("Processing %d,%d" % (u, v))
+        if not (projected or volume):
+            print("nothing to do")
+            return
+        if volume:
+            outfolder_vol = outfolder_base / "volume" / f"chamber_{u}_{v}"
+            outfolder_vol.mkdir(parents=True, exist_ok=True)
+            outfile_vol = outfolder_vol / "dataset.h5"
+            h5_vol_name = str(outfile_vol)
+        if projected:
+            outfolder_proj = outfolder_base / "projection" / f"chamber_{u}_{v}"
+            outfolder_proj.mkdir(parents=True, exist_ok=True)
+            outfile_proj = outfolder_proj / "dataset.h5"
+            h5_proj_name = str(outfile_proj)
+
+        subset = self.df[(self.df.u == u) & (self.df.v == v)]
+
+        save_files_for_bigstitcher(
+            subset.field.values,
+            projected,
+            volume,
+            h5_proj_name=h5_proj_name,
+            h5_vol_name=h5_vol_name,
+            zspacing=zspacing,
+        )
+
+    def process_wells(
+        self,
+        well_indices: List[int],
+        outfolder_base: pathlib.Path,
+        projected: bool = True,
+        volume: bool = False,
+        zspacing: float = 1.0,
+    ):
+        _process = partial(
+            self.process_well,
+            outfolder_base=outfolder_base,
+            projected=projected,
+            volume=volume,
+            zspacing=zspacing,
+        )
+        with ThreadPoolExecutor() as p:
+            return list(p.map(_process, well_indices))
+
+
+def test_populate_file_df():
+    mp = Matrix_Mosaic_Processor("c:/Users/Volker/Data/Testset/")
+    assert not mp.df.empty
+    assert isinstance(mp.uvwells, list)
+
+
+def test_str():
+    mp = Matrix_Mosaic_Processor("c:/Users/Volker/Data/Testset/")
+    print(mp)
+
+
